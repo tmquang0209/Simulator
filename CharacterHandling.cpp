eeb6c9482f0f4a965cd6c7285150eec45448c064
Trần Minh Quang <tmquang0209@gmail.com>
2023-07-07 00:02:14 +0700

    Refactor code
    Refactor the code and add the FileManager class that extends Account

---
+++ CharacterHandling.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include "CharacterHandling.h"
+using namespace std;
+
+bool hasUppercase(const string &str)
+{
+    for (char c : str)
+    {
+        if (isupper(c))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool hasLowercase(const string &str)
+{
+    for (char c : str)
+    {
+        if (islower(c))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool hasDigit(const string &str)
+{
+    for (char c : str)
+    {
+        if (isdigit(c))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void randomDigit(string username)
+{
+    fstream fout;
+    fout.open("VerifyCode.txt", ios::app);
+    int r;
+    string code;
+    srand(time(NULL));
+    for (int i = 0; i < 8; i++)
+    {
+        r = rand() % 26;
+        code += (rand() % 2 == 0) ? 'a' + r : 'A' + r;
+    }
+    fout << username << "|" << code << endl;
+    fout.close();
+}
---
+++ Draw.h
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+#ifndef DRAW_H
+#define DRAW_H
+void gotoxy(int x, int y);
+void drawBox(int x, int y, int width, int height);
+void drawCheckbox(bool isChecked, const string &label);
+#endif
