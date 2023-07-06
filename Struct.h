cb3d4d64357008db08335b11a3f8798d3ff584ed
Trần Minh Quang <tmquang0209@gmail.com>
2023-07-07 00:02:14 +0700

    Refactor code
    Refactor the code and add the FileManager class that extends Account

---
+++ Struct.h
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace std;
+
+#ifndef STRUCT_H
+#define STRUCT_H
+
+struct UserInfo
+{
+    string fullName;
+    string email;
+    string phoneNumber;
+    string username;
+    string password;
+    string typeAccount;
+    bool isDisable;
+    bool changePassword;
+};
+
+struct Permission
+{
+    vector<string> viewers;
+    vector<string> editors;
+    vector<string> deleters;
+    vector<string> renamers;
+};
+
+struct FileInfo
+{
+    string fileName;
+    string fileSize;
+    string authorName;
+    string createdDate;
+    vector<string> content;
+};
+
+#endif
